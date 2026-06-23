7526e4c2d41d5a5f1d5e182506a6f1748fb66242
Banking simulations <ladimp11@gmail.com>
2026-06-23 20:42:24 +0300

    Update Account.cpp

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,1 +1,42 @@
+#include "../include/Account.h"
 
+Account::Account(int accId,
+                 int custId,
+                 double bal,
+                 const std::string& curr)
+{
+    accountId = accId;
+    customerId = custId;
+    balance = bal;
+    currency = curr;
+}
+
+int Account::getAccountId() const
+{
+    return accountId;
+}
+
+int Account::getCustomerId() const
+{
+    return customerId;
+}
+
+double Account::getBalance() const
+{
+    return balance;
+}
+
+void Account::deposit(double amount)
+{
+    if (amount > 0)
+        balance += amount;
+}
+
+bool Account::withdraw(double amount)
+{
+    if (amount > balance || amount <= 0)
+        return false;
+
+    balance -= amount;
+    return true;
+}
