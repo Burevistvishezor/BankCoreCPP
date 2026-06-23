7585685b58b44591fccd5b780b6a59f8aac32791
Banking simulations <ladimp11@gmail.com>
2026-06-23 20:45:05 +0300

    Update Customer.h

--- src/Customer.h
+++ src/Customer.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include <string>
+
+class Customer
+{
+private:
+    int customerId;
+    std::string firstName;
+    std::string lastName;
+
+public:
+    Customer(int id,
+             const std::string& first,
+             const std::string& last);
+
+    int getId() const;
+    std::string getFullName() const;
+};
