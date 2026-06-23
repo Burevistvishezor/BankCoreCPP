12d2b8c18acbfcd966b019f1369bcbae929224e4
Banking simulations <ladimp11@gmail.com>
2026-06-23 20:40:11 +0300

    Update Account.h

--- include/Account.h
+++ include/Account.h
@@ -1,1 +1,25 @@
-Account.h
+#pragma once
+
+#include <string>
+
+class Account
+{
+private:
+    int accountId;
+    int customerId;
+    double balance;
+    std::string currency;
+
+public:
+    Account(int accId,
+            int custId,
+            double bal,
+            const std::string& curr);
+
+    int getAccountId() const;
+    int getCustomerId() const;
+    double getBalance() const;
+
+    void deposit(double amount);
+    bool withdraw(double amount);
+};
